84dc1c536ed47ba89836d1412bcc2c54e6327d8c
DagmawitG <se.dagmawit.getachew@gmail.com>
2021-03-25 22:45:25 +0300

    Bugs fixed

--- project_restaurant/project_restaurant/settings.py
+++ project_restaurant/project_restaurant/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'restaurant.apps.RestaurantConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
