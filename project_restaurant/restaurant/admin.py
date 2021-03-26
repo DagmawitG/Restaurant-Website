9665f15fab24d545825a8e19910b56755b5bdf22
DagmawitG <se.dagmawit.getachew@gmail.com>
2021-03-26 11:19:08 +0300

    Menu Page done

--- project_restaurant/restaurant/admin.py
+++ project_restaurant/restaurant/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Hero,About,WhyUs
+from .models import *
 
 admin.site.register(Hero)
 admin.site.register(About)
 admin.site.register(WhyUs)
+admin.site.register(StarterMenu)
+admin.site.register(MainMenu)
+admin.site.register(DessertMenu)
+admin.site.register(DrinksMenu)
 
--- project_restaurant/restaurant/views.py
+++ project_restaurant/restaurant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from .models import Hero,About,WhyUs
+from .models import *
 
 def home(request):
     context1 = {
@@ -8,7 +8,12 @@ def home(request):
 
         'aboutUs': About.objects.all(),
 
-        'whyUs': WhyUs.objects.all()
+        'whyUs': WhyUs.objects.all(),
+        'starterMenu': StarterMenu.objects.all(),
+        'mainMenu': MainMenu.objects.all(),
+        'dessertMenu': DessertMenu.objects.all(),
+        'drinksMenu': DrinksMenu.objects.all()
+
     }
     
     return render(request, 'restaurant/home.html',context1)
