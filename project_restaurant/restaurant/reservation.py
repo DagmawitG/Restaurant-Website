ae831ad7d21de150fface3e22d0eef2cfca9550b
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-01 01:11:05 +0300

    Conflict resolved

--- project_restaurant/restaurant/reservation.py
+++
@@ -1,8 +0,0 @@
-# from djreservation.views import ProductReservationView
-# from .models import Reservation
-
-# class MyObjectReservation(ProductReservationView):
-#         base_model = Reservation     # required
-#         amount_field = 'quantity' # required
-#         extra_display_field = ['measurement_unit'] # not required
-
--- project_restaurant/restaurant/urls.py
+++ project_restaurant/restaurant/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from djreservation import urls as djreservation_urls
 from . import views
-from . import reservation
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='restaurant-home'),
-    path("reserve", reservation.MyObjectReservation.as_view()),
     path('reservation', views.reservation, name='reservation'),
     path('order/', views.Order.as_view(), name='order'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
