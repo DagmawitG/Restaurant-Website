3342dc857ce7b00bdefecd96fa5833be3e6b2b0d
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-04-01 23:28:24 +0300

    Another one

--- project_restaurant/project_restaurant/settings.py
+++ project_restaurant/project_restaurant/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,"_staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"/media")
