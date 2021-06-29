080ee113d55a7a92a2a7f58fe293adcf1baf11bd
Elshadai <elshadaikassutegegn@gmail.com>
2021-06-29 21:51:13 +0300

    Password changed to app password

--- project_restaurant/project_restaurant/settings.py
+++ project_restaurant/project_restaurant/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'el.delicious.d.etiopia@gmail.com'
-EMAIL_HOST_PASSWORD = 'helloElDeliciousD'
+EMAIL_HOST_PASSWORD = 'lyzqnuwbaqoarlwb'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
