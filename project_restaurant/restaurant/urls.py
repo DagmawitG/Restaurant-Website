919cb201d828ccefd44755a5d250c02904314bac
Elshadai <elshadaikassutegegn@gmail.com>
2021-07-24 13:05:54 +0300

    Bug fix

--- project_restaurant/restaurant/urls.py
+++ project_restaurant/restaurant/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('paypal/', include('paypal.standard.ipn.urls')),
     path('order/', views.Order.as_view(), name='order'),
     path('google9f1fe2c472c358f0.html/', views.verify, name='verify'),
-    path('/*', views.home, name='restaurant-home'),
+    path('^$', views.home, name='restaurant-home'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
