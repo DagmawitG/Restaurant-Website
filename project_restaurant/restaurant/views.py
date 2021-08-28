27651d6fea2ef35c4573e80fd2302cc0a32ffd72
Elshadai <elshadaikassutegegn@gmail.com>
2021-08-28 16:44:46 +0300

    Sitemap created

--- project_restaurant/restaurant/views.py
+++ project_restaurant/restaurant/views.py
@@ -240,6 +240,9 @@ def home(request):
 def verify(request):
     return render(request, 'restaurant/google9f1fe2c472c358f0.html')
 
+def site(request):
+    return render(request, 'restaurant/sitemap.xml')
+
 def reservation(request):
     
    
