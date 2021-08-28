466fb9b08b60912356a4360e970bd77ce8a9a3cb
Elshadai <elshadaikassutegegn@gmail.com>
2021-08-28 17:38:57 +0300

    rolled back again

--- project_restaurant/restaurant/urls.py
+++ project_restaurant/restaurant/urls.py
@@ -5,12 +5,7 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.contrib.sitemaps.views import sitemap
-from . import sitemap
-StaticSitemap = sitemap.StaticSitemap
-sitemaps = {
-    'static': StaticSitemap,
-}
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='restaurant-home'),
@@ -20,9 +15,7 @@ urlpatterns = [
     path('paypal/', include('paypal.standard.ipn.urls')),
     path('order/', views.Order.as_view(), name='order'),
     path('google9f1fe2c472c358f0.html/', views.verify, name='verify'),
-        path('sitemap.xml', sitemap,
-         {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml/', TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
     path('^$', views.home, name='restaurant-home'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
